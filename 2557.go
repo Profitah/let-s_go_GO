ad788aeb379bf907215c9e3309483682852775bc
Roselle Yoonah Lee <101340482+Profitah@users.noreply.github.com>
2025-11-04 23:36:58 +0900

    2557
    https://www.acmicpc.net/source/99971339

---
+++ 2557.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt" // Go 표준 라이브러리에서 표준 포맷 입출력 패키지를 import
+
+func main() {
+    fmt.Println("Hello World!")
+}
+
+
+/*
+묘하게 문법이 C스러워서 찾아보니,
+Go를 만든 사람 중 한명이 켄 톰프슨(Ken Thompson)이었다.
+이 사람은 C 언어를 만든 사람이기도 해서,
+이 사람에 의해 Go가 C의 영향을 받은 듯 하다.
+*/
